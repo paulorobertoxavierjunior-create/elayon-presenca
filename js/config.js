c250c917c886ba85ea4d92ac8d5ed9f354ec8c0b
paulorobertoxavierjunior-create <paulorobertoxavierjunior@gmail.com>
2026-04-08 22:51:02 -0400

    Criar js/config.js

---
+++ js/config.js
@@ -0,0 +1,19 @@
+window.ELAYON_CONFIG = {
+  appName: "Elayon Presença",
+  trialDays: 7,
+  reportLionPrice: 0.99,
+  routes: {
+    index: "index.html",
+    login: "login.html",
+    cadastro: "cadastro.html",
+    painel: "painel.html",
+    presenca: "presenca.html",
+    relatorioLion: "relatorio-lion.html",
+    manutencao: "manutencao.html"
+  },
+  storageKeys: {
+    session: "elayon_session",
+    trial: "elayon_trial",
+    report: "elayon_report"
+  }
+};
