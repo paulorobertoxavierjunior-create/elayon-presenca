e2af3aa53efdf23b37ec92dabf1d31b1d94416bd
paulorobertoxavierjunior-create <paulorobertoxavierjunior@gmail.com>
2026-04-12 14:35:28 -0400

    Atualizar o painel.js

--- js/painel.js
+++ js/painel.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://eudcjihffrfmhzmfwtlg.supabase.co";
-const SUPABASE_KEY = "COLE_AQUI_A_ANON_KEY_COMPLETA";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1ZGNqaWhmZnJmbWh6bWZ3dGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3NDE3MjUsImV4cCI6MjA5MDMxNzcyNX0.2tod6vvl_4SAXzSmW1wU8Mk9pLn8fvhF2xrAZOysUu0";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
